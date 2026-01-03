d0cf1c7f66d47c6e14181d2a7957d2aeeadefb27
infosanuth <infosanuth@gmail.com>
2026-01-03 15:33:41 +0530

    Implement job data fetching in ApplyJob page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const ApplyJob = () => {
+
+  const { id } = useParams()
+
+  const [jobData, setJobData] = useState(null)
+
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id === id)
+    if(data.length !== 0){
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob()
+    }
+  },[id,jobs])
+
+
   return (
     <div>
-      ApplyJob
+      
     </div>
   )
 }
