cd29631897ef891c200d1bd056af4c61043c50b5
infosanuth <infosanuth@gmail.com>
2026-01-05 15:04:16 +0530

    Add sidebar to ApplyJob page showing other jobs from same company

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
 
   const { openSignIn } = useClerk()
   const { user  } = useUser();
 
+  const navigate = useNavigate()
+
   return (
     <div className='shadow py-4'>
       <div className='container px-4 2xl:px20 mx-auto flex justify-between items-center'>
-        <img src={assets.logo} alt="" />
+        <img onClick={()=> navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
 
 
         {user  ? (
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -6,6 +6,8 @@ import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 import kconvert from 'k-convert'
 import moment from 'moment'
+import JobCard from '../components/JobCard'
+import Footer from '../components/Footer'
 
 const ApplyJob = () => {
 
@@ -74,11 +76,21 @@ const ApplyJob = () => {
               <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
               <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
             </div>
+
+            {/* Right section More Jobs */}
+            <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-6'>
+              <h2>More jobs from {jobData.companyId.name}</h2>
+
+              {jobs.filter( job => job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+              .filter(job => true).slice(0,4)
+              .map((job,index)=> <JobCard key={index} job={job}/>)}
+            </div>
+
           </div>
 
         </div>
       </div>
-
+      <Footer/>
     </>
   ) : (
     <Loading />
