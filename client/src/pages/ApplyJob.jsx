0732ea0a3e071fb268dc901a66370bf7a18430e8
infosanuth <infosanuth@gmail.com>
2026-01-05 14:38:32 +0530

    Display selected job data on ApplyJob page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -66,7 +66,16 @@ const ApplyJob = () => {
               <p className='mt-1 text-gray-600'>Posted {moment(jobData.date).fromNow()}</p>
             </div>
 
-          </div>    
+          </div>  
+
+          <div className='flex flex-col lg:flex-row justify-baseline items-start'>
+            <div className='w-full lg:w-2/3'>
+              <h2 className='font-bold text-2xl mb-4'>Job description</h2>
+              <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
+              <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
+            </div>
+          </div>
+
         </div>
       </div>
 
