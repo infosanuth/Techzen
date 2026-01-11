f822e2a7d6f2576c3698619b56dbc4213f321cf8
infosanuth <infosanuth@gmail.com>
2026-01-11 21:52:08 +0530

    Improve recruiter login form UX

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const RecruiterLogin = () => {
 
@@ -13,6 +14,8 @@ const RecruiterLogin = () => {
 
   const [isTextDataSubmited, setIsTextDataSubmited] = useState(false)
 
+  const {setShowRecruiterLogin} = useContext(AppContext)
+
   const onSubmitHandler = async (e) => {
     e.preventDefault()
 
@@ -21,6 +24,13 @@ const RecruiterLogin = () => {
       setIsTextDataSubmited(true)
     }
   }
+
+  useEffect(()=>{
+    document.body.style.overflow = 'hidden'
+    return () =>{
+      document.body.style.overflow = 'unset'
+    }
+  },[])
 
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
@@ -68,6 +78,8 @@ const RecruiterLogin = () => {
             ? <p className='mt-5 text-center'>Don't have an account <span className='text-blue-600 cursor-pointer' onClick={() => setState("Sign up")}>Sign up</span></p>
             : <p className='mt-5 text-center'>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={() => setState("Login")}>Login</span></p>
         }
+
+        <img onClick={e => setShowRecruiterLogin(false)} className='absolute top-5 right-5 cursor-pointer' src={assets.cross_icon} alt="" />
 
       </form>
     </div>
