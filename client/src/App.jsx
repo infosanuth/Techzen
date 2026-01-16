303ba390661bfc987c11b73433d76c1965629e2c
infosanuth <infosanuth@gmail.com>
2026-01-16 22:13:39 +0530

    Add recruiter pages and configure routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,34 @@ import Applications from './pages/Applications'
 import AllJobs from './pages/AllJobs'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplication from './pages/ViewApplication'
+import ApplicantProfile from './pages/ApplicantProfile'
+import Settings from './pages/Settings'
 
 const App = () => {
 
-  const {showRecruiterLogin} = useContext(AppContext)
+  const { showRecruiterLogin } = useContext(AppContext)
 
   return (
     <div>
       {showRecruiterLogin && < RecruiterLogin />}
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/apply-job/:id' element={<ApplyJob/>}/>
-          <Route path='/applications' element={<Applications/>}/>
-          <Route path='/jobs' element={<AllJobs/>} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
+        <Route path='/applications' element={<Applications />} />
+        <Route path='/jobs' element={<AllJobs />} />
+
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='manage-job' element={<ManageJobs />} />
+          <Route path='view-applications' element={<ViewApplication />} />
+          <Route path='applicant-profile' element={<ApplicantProfile />} />
+          <Route path='settings' element={<Settings />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/ViewApplication.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewApplication = () => {
+  return (
+    <div>
+      ViewApplication
+    </div>
+  )
+}
+
+export default ViewApplication
