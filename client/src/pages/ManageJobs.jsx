d5ffec94e5cffa0e00af3c762739c390ad5dd34d
infosanuth <infosanuth@gmail.com>
2026-01-22 23:11:42 +0530

    Implement ManageJobs page structure

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { manageJobData } from '../assets/assets';
+import moment from 'moment';
 
 const ManageJobs = () => {
   return (
     <div>
-      ManageJobs
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Job Title</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Applicants</th>
+              <th>Visible</th>
+            </tr>
+          </thead>
+          <tbody>
+            {manageJobData.map((job, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{job.title}</td>
+                <td>{moment(job.date).format('ll')}</td>
+                <td>{job.location}</td>
+                <td>{job.applicants}</td>
+                <td><input type="checkbox" /></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageJobs
+export default ManageJobs;
+
