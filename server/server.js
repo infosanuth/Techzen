6533cf75ba8db5194956bf385f25a65da0149c8e
infosanuth <infosanuth@gmail.com>
2026-02-02 19:38:50 +0530

    Add User model schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 
 // Initialize Express
 const app = express()
+
+// Connect to database
+await connectDB()
 
 
 // Middlewares
