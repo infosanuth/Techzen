e988684195d9859e2d94839d4494ac84865a4bcc
infosanuth <infosanuth@gmail.com>
2026-02-14 13:16:54 +0530

    feat: add company login route

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { registerCompany } from '../controllers/companyController.js'
+import { registerCompany, loginCompany } from '../controllers/companyController.js'
 import upload from '../config/multer.js'
 
 const router = express.Router()
 
 // Register a company
 router.post('/register', upload.single('image'), registerCompany)
+
+// Company login
+router.post('/login', loginCompany)
 
 export default router
