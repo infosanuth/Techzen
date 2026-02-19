5c4901326e5dd4820c3eb44597a12f38a3e64d06
infosanuth <infosanuth@gmail.com>
2026-02-19 22:43:09 +0530

    feat: add route for company job posting

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { registerCompany, loginCompany } from '../controllers/companyController.js'
+import { registerCompany, loginCompany, getCompanyData, postJob } from '../controllers/companyController.js'
 import upload from '../config/multer.js'
+import { protectCompany } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
@@ -9,5 +10,11 @@ router.post('/register', upload.single('image'), registerCompany)
 
 // Company login
 router.post('/login', loginCompany)
+
+// Get company data
+router.get('/company', protectCompany, getCompanyData)
+
+// Post a job
+router.post('/post-job', protectCompany, postJob)
 
 export default router
