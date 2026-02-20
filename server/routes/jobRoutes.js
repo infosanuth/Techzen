6c52c4503d26a55af23f7936ce3ded5f2ae608d8
infosanuth <infosanuth@gmail.com>
2026-02-20 22:36:57 +0530

    feat: add route to get job by ID

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getJobs } from '../controllers/jobController.js';
+import { getJobByID, getJobs } from '../controllers/jobController.js';
 
 const router = express.Router()
 
 // Route to get all jobs data
 router.get('/',getJobs)
+
+// Route to get a single job by ID
+router.get('/:id',getJobByID)
 
 export default router;
