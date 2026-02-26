795d6be7eb087459cd3c2a8cbdd46b84702224f6
infosanuth <infosanuth@gmail.com>
2026-02-26 22:29:04 +0530

    feat: setup Clerk authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhook } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
+import {clerkMiddleware } from '@clerk/express'
 
 
 // Initialize Express
@@ -25,6 +26,7 @@ app.post(
 // Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.use('/api/company', companyRoutes)
 app.use('/api/jobs', jobRoutes)
