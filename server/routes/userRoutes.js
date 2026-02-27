a9e0f89b99df58a47407a663edcfc50dbe80cbdb
infosanuth <infosanuth@gmail.com>
2026-02-27 22:24:43 +0530

    feat: add route for applying to a job

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import {  getUserData} from '../controllers/userController.js'
+import { applyForJob, getUserData, getUserJobApplication, updateUserResume } from '../controllers/userController.js'
 
 const router = express.Router()
 
 // Get user data
 router.get('/user', getUserData)
+
+// Apply for a job
+router.post('/apply', applyForJob)
 
 
 export default router
