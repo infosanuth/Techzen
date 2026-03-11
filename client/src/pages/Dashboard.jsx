7e701019dbe152ff1b38ed63da31f4bab38ac7ad
infosanuth <infosanuth@gmail.com>
2026-03-11 14:13:12 +0530

    feat: display logged-in company data in navbar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { MdOutlineLibraryAdd } from "react-icons/md"
 import { FiHome } from "react-icons/fi"
 import { RiListView } from "react-icons/ri";
 import { VscSettingsGear } from "react-icons/vsc";
+import { AppContext } from '../context/AppContext'
 
 const Dashboard = () => {
 
   const navigate = useNavigate()
 
+  const { companydata } = useContext(AppContext)
+
   return (
     <div className='min-h-screen'>
 
       {/* Navbar for Recuriter Panel */}
-      <div className='shadow py-4'>
-        <div className='px-5 flex justify-between items-center'>
-          <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
-          <div className='flex items-center gap-3'>
-            <p className='max-sm:hidden'>Welcome, LSEG</p>
-            <div className='relative group'>
-              <img className='w-8 border border-white rounded-full' src={assets.company_icon} alt="" />
-              <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-                <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                  <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
-                </ul>
+      <div className='py-4 shadow'>
+        <div className='flex items-center justify-between px-5'>
+          <img onClick={e => navigate('/')} className='cursor-pointer max-sm:w-32' src={assets.logo} alt="" />
+          {companydata && (
+            <div className='flex items-center gap-3'>
+              <p className='max-sm:hidden'>Welcome, {companydata.name}</p>
+              <div className='relative group'>
+                <img className='w-8 border border-white rounded-full' src={companydata.image} alt="" />
+                <div className='absolute top-0 right-0 z-10 hidden pt-12 text-black rounded group-hover:block'>
+                  <ul className='p-2 m-0 text-sm list-none bg-white border rounded-md'>
+                    <li className='px-2 py-1 pr-10 cursor-pointer'>Logout</li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
 
@@ -45,11 +50,11 @@ const Dashboard = () => {
               <p className='max-sm:hidden'>Manage Job</p>
             </NavLink>
             <NavLink className={({ isActive }) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
-              <RiListView className='size-6'/>
+              <RiListView className='size-6' />
               <p className='max-sm:hidden'>View Applications</p>
             </NavLink>
             <NavLink className={({ isActive }) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/settings'}>
-              <VscSettingsGear className='size-6'/>
+              <VscSettingsGear className='size-6' />
               <p className='max-sm:hidden'>Settings</p>
             </NavLink>
           </ul>
