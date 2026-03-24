f150642bc65a08721afda3f158e00faf6bb32c54
infosanuth <infosanuth@gmail.com>
2026-03-24 21:46:47 +0530

    feat: implement fetchUserApplications function

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -92,6 +92,26 @@ export const AppContextProvider = (props) => {
         }
     }
 
+    // Function to fetch user's applied applications data
+    const fetchUserApplications = async () => {
+        try {
+
+            const token = await getToken()
+
+            const { data } = await axios.get(backendUrl + '/api/users/applications',
+                { headers: { Authorization : `Bearer ${token}`} }
+            )
+            if(data.success) {
+                setUserApplication(data.applications)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
 
     useEffect(() => {
         fetchJobs()
@@ -113,6 +133,7 @@ export const AppContextProvider = (props) => {
     useEffect(() => {
         if (user) {
             fetchUserData()
+            fetchUserApplications()
         }
     }, [user])
 
@@ -125,7 +146,9 @@ export const AppContextProvider = (props) => {
         companydata, setCompanyData,
         backendUrl,
         userData, setUserData,
-        userApplication, setUserApplication
+        userApplication, setUserApplication,
+        fetchUserData,
+        fetchUserApplications
     }
 
     return (<AppContext.Provider value={value}>
