2c801ad2361beda3d72dcc0039b1490d1b36256c
infosanuth <infosanuth@gmail.com>
2026-04-07 09:44:51 +0530

    feat: show message when no jobs are available

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -10,7 +10,7 @@ const ManageJobs = () => {
 
   const navigate = useNavigate()
 
-  const [jobs, setJobs] = useState([])
+  const [jobs, setJobs] = useState(false)
 
   const { backendUrl, companyToken } = useContext(AppContext)
 
@@ -64,41 +64,45 @@ const ManageJobs = () => {
     }
   }, [companyToken])
 
-  return (
-    <div className='container max-w-5xl p-4'>
-      <div className='overflow-x-auto'>
-        <table className='min-w-full bg-white border border-gray-200 max-sm:text-sm'>
-          <thead>
-            <tr>
-              <th className='px-4 py-2 text-left border-b max-sm:hidden'>#</th>
-              <th className='px-4 py-2 text-left border-b'>Job Title</th>
-              <th className='px-4 py-2 text-left border-b max-sm:hidden'>Date</th>
-              <th className='px-4 py-2 text-left border-b max-sm:hidden'>Location</th>
-              <th className='px-4 py-2 text-center border-b'>Applicants</th>
-              <th className='px-4 py-2 text-left border-b'>Visible</th>
-            </tr>
-          </thead>
-          <tbody>
-            {jobs.map((job, index) => (
-              <tr key={index} className='text-gray-700'>
-                <td className='px-4 py-2 border-b max-sm:hidden'>{index + 1}</td>
-                <td className='px-4 py-2 border-b'>{job.title}</td>
-                <td className='px-4 py-2 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                <td className='px-4 py-2 border-b max-sm:hidden'>{job.location}</td>
-                <td className='px-4 py-2 text-center border-b'>{job.applicants}</td>
-                <td className='px-4 py-2 border-b'>
-                  <input onChange={() => changeJobVisibility(job._id)} className='ml-4 scale-125' type="checkbox" checked={job.visible} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className='flex justify-end mt-4'>
-        <button onClick={() => navigate('/dashboard/add-job')} className='px-4 py-2 text-white bg-black rounded'>Add new job</button>
-      </div>
+  return jobs ? jobs.length === 0 ?
+    (<div className='flex items-center justify-center h-[70vh]'>
+      <p className='text-xl sm:text-2xl'>No Jobs Available or posted</p>
     </div>
-  );
+    ) : (
+      <div className='container max-w-5xl p-4'>
+        <div className='overflow-x-auto'>
+          <table className='min-w-full bg-white border border-gray-200 max-sm:text-sm'>
+            <thead>
+              <tr>
+                <th className='px-4 py-2 text-left border-b max-sm:hidden'>#</th>
+                <th className='px-4 py-2 text-left border-b'>Job Title</th>
+                <th className='px-4 py-2 text-left border-b max-sm:hidden'>Date</th>
+                <th className='px-4 py-2 text-left border-b max-sm:hidden'>Location</th>
+                <th className='px-4 py-2 text-center border-b'>Applicants</th>
+                <th className='px-4 py-2 text-left border-b'>Visible</th>
+              </tr>
+            </thead>
+            <tbody>
+              {jobs.map((job, index) => (
+                <tr key={index} className='text-gray-700'>
+                  <td className='px-4 py-2 border-b max-sm:hidden'>{index + 1}</td>
+                  <td className='px-4 py-2 border-b'>{job.title}</td>
+                  <td className='px-4 py-2 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                  <td className='px-4 py-2 border-b max-sm:hidden'>{job.location}</td>
+                  <td className='px-4 py-2 text-center border-b'>{job.applicants}</td>
+                  <td className='px-4 py-2 border-b'>
+                    <input onChange={() => changeJobVisibility(job._id)} className='ml-4 scale-125' type="checkbox" checked={job.visible} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className='flex justify-end mt-4'>
+          <button onClick={() => navigate('/dashboard/add-job')} className='px-4 py-2 text-white bg-black rounded'>Add new job</button>
+        </div>
+      </div>
+    ) : <></>
 };
 
 export default ManageJobs;
